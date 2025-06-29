add071df66f22b1dbfcfe2764b921e79bb66c660
priom20-rgb <sargomkcg@gmail.com>
2025-06-29 20:16:01 +0600

    Create Taskone.java

---
+++ LabOne/Taskone.java
@@ -0,0 +1,11 @@
+class Main
+{
+public static void main(String[] args) {
+String name = "Md Sabid Amed";
+String studentId = "E243034";
+String email = "ahmedsabid89@gmail.com";
+String number = "01937584028";
+String address = "Chawk bazar";
+System.out.println("Name: " + name +"\nStudent Id: "+ studentId + "\nEmail: "+email +"\nPhone: "+ number +"\nAddress: "+ address);
+}
+}
